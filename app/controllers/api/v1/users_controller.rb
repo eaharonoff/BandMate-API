7cd876e41a75294675a543dd5c71a46d4956f932
eaharonoff <emanuel.aharonoff@gmail.com>
2016-08-13 20:29:07 -0400

    user api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class UsersController < ApplicationController
       def index
-        render json: User.includes(:city), include: ['city']
+        render json: User.includes(:city, :instruments, :genres), include: ['city', 'instruments', 'genres']
       end
     end
   end
