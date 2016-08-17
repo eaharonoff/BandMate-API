6659f1f1c7431955fe6e558c495e5d4a5087caaa
leearaneta <lee.araneta@gmail.com>
2016-08-17 14:32:38 -0400

    refactored reducers

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -4,15 +4,16 @@ class FriendRequestsController < ApplicationController
     real_params = JSON.parse(params.keys[0])
     sender = User.find(real_params['myId'])
     recipient = User.find(real_params['userId'])
-    FriendRequest.create(sender: sender, recipient: recipient)
-    render json: sender, include: ["name", "zip", "age", "bio", "genres", "instruments", "sent_requests", "sent_requests.recipient", "received_requests", "received_requests.sender", "friends", "friends.genres", "friends.instruments", "inverse_friends", "inverse_friends.genres", "inverse_friends.instruments"]
+    friend_request = FriendRequest.create(sender: sender, recipient: recipient)
+    render json: friend_request, include: ['recipient.id']
   end
 
   def delete
     real_params = JSON.parse(params.keys[0])
     sender = User.find(real_params['senderId'])
     recipient = User.find(real_params['userId'])
-    FriendRequest.where('sender_id = ? AND recipient_id = ?', sender.id, recipient.id).destroy_all
-    render json: recipient, include: ["name", "zip", "age", "bio", "genres", "instruments", "sent_requests", "sent_requests.recipient", "received_requests", "received_requests.sender", "friends", "friends.genres", "friends.instruments", "inverse_friends", "inverse_friends.genres", "inverse_friends.instruments"]
+    friend_request = FriendRequest.find_by(sender: sender, recipient: recipient)
+    render json: friend_request, include: ['recipient.id']
   end
+
 end
