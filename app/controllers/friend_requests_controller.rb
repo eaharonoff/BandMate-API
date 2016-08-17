404eed4af136960ff32ff6f7efd3590692aadf48
leearaneta <lee.araneta@gmail.com>
2016-08-17 04:22:10 -0400

    friend stuff

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,2 +1,18 @@
 class FriendRequestsController < ApplicationController
+
+  def create
+    real_params = JSON.parse(params.keys[0])
+    sender = User.find(real_params['myId'])
+    recipient = User.find(real_params['friendId'])
+    FriendRequest.create(sender: sender, recipient: recipient)
+    render json: sender, include: ["name", "zip", "age", "bio", "genres", "instruments", "sent_requests", "sent_requests.recipient", "received_requests", "received_requests.sender", "friends", "friends.genres", "friends.instruments", "inverse_friends", "inverse_friends.genres", "inverse_friends.instruments"]
+  end
+
+  def delete
+    real_params = JSON.parse(params.keys[0])
+    sender = User.find(real_params['senderId'])
+    recipient = User.find(real_params['userId'])
+    FriendRequest.where('sender_id = ? AND recipient_id = ?', sender.id, recipient.id).destroy_all
+    render json: recipient, include: ["name", "zip", "age", "bio", "genres", "instruments", "sent_requests", "sent_requests.recipient", "received_requests", "received_requests.sender", "friends", "friends.genres", "friends.instruments", "inverse_friends", "inverse_friends.genres", "inverse_friends.instruments"]
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :zip, :age, :bio, :all_friends
+  attributes :id, :name, :email, :zip, :age, :bio
   has_many :instruments
   has_many :genres
+  has_many :sent_requests
+  has_many :received_requests
+  has_many :friends
+  has_many :inverse_friends
 end
