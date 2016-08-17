e8d694d1db8b67bb8184ac54abb344e5c4b98e87
leearaneta <lee.araneta@gmail.com>
2016-08-17 12:09:17 -0400

    refactored basics

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -3,7 +3,7 @@ class FriendRequestsController < ApplicationController
   def create
     real_params = JSON.parse(params.keys[0])
     sender = User.find(real_params['myId'])
-    recipient = User.find(real_params['friendId'])
+    recipient = User.find(real_params['userId'])
     FriendRequest.create(sender: sender, recipient: recipient)
     render json: sender, include: ["name", "zip", "age", "bio", "genres", "instruments", "sent_requests", "sent_requests.recipient", "received_requests", "received_requests.sender", "friends", "friends.genres", "friends.instruments", "inverse_friends", "inverse_friends.genres", "inverse_friends.instruments"]
   end
