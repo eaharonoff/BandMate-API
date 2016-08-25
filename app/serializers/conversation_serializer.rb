efeb75512786378ba56d89142d7b3b23de1db7d2
eaharonoff <emanuel.aharonoff@gmail.com>
2016-08-25 11:50:26 -0400

    added attributes to conversation serializer

--- app/serializers/conversation_serializer.rb
+++ app/serializers/conversation_serializer.rb
@@ -2,5 +2,5 @@ class ConversationSerializer < ActiveModel::Serializer
   has_many :messages
   belongs_to :user1, class_name: 'User'
   belongs_to :user2, class_name: 'User'
-  attributes :id, :messages
+  attributes :id, :messages, :user1_name, :user1_id, :user2_id, :user2_name
 end
